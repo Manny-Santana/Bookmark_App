dfbce7497a42f6f115bcb83a3adb2482d7fc0dcb
Manny-Santana <aresxwarlord@outlook.com>
2019-11-19 18:06:07 -0500

    made seymur happy

--- bookmarks/src/App.js
+++ bookmarks/src/App.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <h1>Heres the Fucking front end Seymur</h1>
+    </div>
+  );
 }
 
 export default App;
