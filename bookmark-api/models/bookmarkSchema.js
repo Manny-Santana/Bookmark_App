c70fefff363662e342578a8d10364ac3031908dc
Seymur <smammadov94@gmail.com>
2019-11-19 17:57:58 -0500

    new schema

--- bookmark-api/models/bookmarkSchema.js
+++ bookmark-api/models/bookmarkSchema.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const bookmarkSchema = mongoose.Schema({
   name: { type: String, required: true },
+  link: { type: String, required: true },
   description: { type: String, default: "Visited website." },
   favorite: { type: Boolean, default: "false" }
 });
